c6d523581dbaf7230df6b28ff5ea22ab049808f5
bmwasonga <bmwasonga@gmail.com>
2021-04-13 23:29:02 +0300

    part 2 done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,28 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/Searchbar';
 
+//business list
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90,
+};
+
+//businesses array
+const businesses = [business, business, business, business, business, business];
+
 function App() {
   return (
     <div className="App">
-      <h1>ravenous</h1>
+      <h1>Ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses} />
     </div>
   );
 }

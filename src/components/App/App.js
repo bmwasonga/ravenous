d30b88d37f1e9c1db8a53cb3008e52eb64d5e675
bmwasonga <bmwasonga@gmail.com>
2021-04-14 00:44:28 +0300

    completed part 2 with errors

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/Searchbar';
+import React from 'react';
 
 //business list
 const business = {
@@ -18,14 +19,20 @@ const business = {
 //businesses array
 const businesses = [business, business, business, business, business, business];
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Ravenous</h1>
-      <SearchBar />
-      <BusinessList businesses={businesses} />
-    </div>
-  );
+class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`you are looking up ${term}, ${location} and ${sortBy}`);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={businesses} />
+      </div>
+    );
+  }
 }
 
 export default App;
